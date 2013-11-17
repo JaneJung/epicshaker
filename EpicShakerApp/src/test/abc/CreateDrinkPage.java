70ce1e237345ac14a3d59b36bfcfbf0e8232536f
dimitree <jaurim1216@gmail.com>
2013-11-17 21:34:24 +0900

    애니메이션 수정

--- EpicShakerApp/src/test/abc/CreateDrinkPage.java
+++ EpicShakerApp/src/test/abc/CreateDrinkPage.java
@@ -22,86 +22,135 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
- 
+import android.widget.RelativeLayout;
+
 public class CreateDrinkPage extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.createdrinkpage);
-        LinearLayout lay1 = (LinearLayout)findViewById(R.id.layout1);
-               
-        View waterView= new View(this);
-        waterView.setBackgroundColor(Color.YELLOW);
-        waterView.setX(227);
-       // waterView.setRight(120);
-        waterView.setY(113);
-        waterView.setAlpha(0.8f);
-        waterView.setLayoutParams(new LayoutParams(200, 400));
-        
-        lay1.addView(waterView);
-        
-        waterView.setPivotY(400);
-        ObjectAnimator imageAnimator = ObjectAnimator.ofFloat(waterView, View.SCALE_Y,
-                1f,0);
-	    imageAnimator.setDuration(5000);
-	    imageAnimator.start();
-	    
-	    //setContentView(waterView);
-        
-    
-    }
-    
-    public void connectArduino() throws IOException{
-    	UsbManager manager = (UsbManager) getSystemService(Context.USB_SERVICE);
-    	
-    	UsbSerialDriver driver = UsbSerialProber.acquire(manager);
-   
-    	if(driver != null) {
-    		driver.open();
-    		try{
-    			driver.setBaudRate(115200);
-    			
-    			byte buffer[] = new byte[16];
-    			int numBytesRead = driver.read(buffer,1000);
-    			EditText text1=(EditText)findViewById(R.id.editText1);
-    			text1.setText(numBytesRead);
-    		} catch (IOException e) {
-    		} finally {
-    			driver.close();
-    		}
-    	}
-   
-    }
+	/** Called when the activity is first created. */
+
+	int waterColor;
+	float curWaterLevel=0;
+	final int cupBottom=113;
+	final int cupHeight=400;
+	
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.createdrinkpage);
+		//LinearLayout lay1 = (LinearLayout)findViewById(R.id.layout1);
+		waterColor = Color.GREEN;
+		curWaterLevel = 0;
+		updateWaterLevel(0.5f);
+		/*
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		*/
+		waterColor = Color.RED;
+		curWaterLevel = 100;
+		updateWaterLevel(1f);
+		/*
+		View waterView= new View(this);
+		waterView.setBackgroundColor(Color.YELLOW);
+		waterView.setX(227);
+		// waterView.setRight(120);
+		waterView.setY(113);
+		waterView.setAlpha(0.8f);
+		waterView.setLayoutParams(new LayoutParams(200, 400));
+
+		lay1.addView(waterView);
+
+		waterView.setPivotY(400);
+		ObjectAnimator imageAnimator = ObjectAnimator.ofFloat(waterView, View.SCALE_Y,
+				1f,0);
+		imageAnimator.setDuration(5000);
+		imageAnimator.start();
+*/
+		//setContentView(waterView);
+
+
+	}
+	
+	public void finishMaterial () {
+		
+	}
+	
+
+	public void updateWaterLevel (float level) {
+		RelativeLayout lay1 = (RelativeLayout)findViewById(R.id.layout1);
+		View waterView= new View(this);
+		waterView.setBackgroundColor(waterColor);
+		waterView.setX(227);
+		// waterView.setRight(120);
+		waterView.setY(cupBottom);
+		waterView.setAlpha(0.8f);
+		waterView.setLayoutParams(new LayoutParams(200, 400));
+
+		lay1.addView(waterView);
+
+		//waterView.setPivotY(400);
+		waterView.setPivotY(cupHeight-curWaterLevel);
+		ObjectAnimator imageAnimator = ObjectAnimator.ofFloat(waterView, View.SCALE_Y,
+				0,level);
+		imageAnimator.setDuration(1000);
+		imageAnimator.start();
+	} 
+
+
+
+	public void connectArduino() throws IOException{
+		UsbManager manager = (UsbManager) getSystemService(Context.USB_SERVICE);
+
+		UsbSerialDriver driver = UsbSerialProber.acquire(manager);
+
+		if(driver != null) {
+			driver.open();
+			try{
+				driver.setBaudRate(115200);
+
+				byte buffer[] = new byte[16];
+				int numBytesRead = driver.read(buffer,1000);
+				EditText text1=(EditText)findViewById(R.id.editText1);
+				text1.setText(numBytesRead);
+			} catch (IOException e) {
+			} finally {
+				driver.close();
+			}
+		}
+
+	}
 
 }
 
 
 class ResizeAnimation extends Animation {
-    private View mView;
-    private float mToHeight;
-    private float mFromHeight;
+	private View mView;
+	private float mToHeight;
+	private float mFromHeight;
 
-    private float mToWidth;
-    private float mFromWidth;
+	private float mToWidth;
+	private float mFromWidth;
 
-    public ResizeAnimation(View v, float fromWidth, float fromHeight, float toWidth, float toHeight) {
-        mToHeight = toHeight;
-        mToWidth = toWidth;
-        mFromHeight = fromHeight;
-        mFromWidth = fromWidth;
-        mView = v;
-        setDuration(300);
-    }
+	public ResizeAnimation(View v, float fromWidth, float fromHeight, float toWidth, float toHeight) {
+		mToHeight = toHeight;
+		mToWidth = toWidth;
+		mFromHeight = fromHeight;
+		mFromWidth = fromWidth;
+		mView = v;
+		setDuration(300);
+	}
 
-    @Override
-    protected void applyTransformation(float interpolatedTime, Transformation t) {
-        float height =
-                (mToHeight - mFromHeight) * interpolatedTime + mFromHeight;
-        float width = (mToWidth - mFromWidth) * interpolatedTime + mFromWidth;
-        LayoutParams p = mView.getLayoutParams();
-        p.height = (int) height;
-        p.width = (int) width;
-        mView.requestLayout();
-    }
+	@Override
+	protected void applyTransformation(float interpolatedTime, Transformation t) {
+		float height =
+				(mToHeight - mFromHeight) * interpolatedTime + mFromHeight;
+		float width = (mToWidth - mFromWidth) * interpolatedTime + mFromWidth;
+		LayoutParams p = mView.getLayoutParams();
+		p.height = (int) height;
+		p.width = (int) width;
+		mView.requestLayout();
+	}
 }
