47cf9f1b5aa2647d89f34cd7a6881add4d59de67
dimitree <jaurim1216@gmail.com>
2013-11-19 16:04:22 +0900

    중간 발표 전 수정

--- EpicShakerApp/src/test/abc/InputDrinkInfoPage.java
+++ EpicShakerApp/src/test/abc/InputDrinkInfoPage.java
@@ -1,28 +1,28 @@
 package test.abc;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 import android.app.Activity;
 import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
-public class CreateDrinkListPage extends Activity {
+public class InputDrinkInfoPage extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.createdrinklistpage);
+		setContentView(R.layout.inputdrinkinfopage);
 		
 		Button btn1=(Button)findViewById(R.id.button1);
 		btn1.setOnClickListener(new Button.OnClickListener() {
 			public void onClick(View v) {
-				Intent intetn2 = new Intent(CreateDrinkListPage.this , InputDrinkInfoPage.class);
+				Intent intetn2 = new Intent(InputDrinkInfoPage.this , CreateDrinkPage.class);
 				startActivity(intetn2);
 			}
 		});
-
 	}
 
-	
-	
+
+
 }
