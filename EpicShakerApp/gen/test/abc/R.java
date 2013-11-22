7f9e47bbba8d7f9d73966cc73dd7c63d7b0a190d
dimitree <jaurim1216@gmail.com>
2013-11-22 18:01:10 +0900

    음료 창작 페이지 서버 업로드 완료

--- EpicShakerApp/gen/test/abc/R.java
+++ EpicShakerApp/gen/test/abc/R.java
@@ -22,27 +22,38 @@ public final class R {
     }
     public static final class drawable {
         public static final int background1=0x7f020000;
-        public static final int buttonstyle=0x7f020001;
-        public static final int cupborder=0x7f020002;
-        public static final int ic_launcher=0x7f020003;
-        public static final int skaqkrtk_1285=0x7f020004;
+        public static final int bm=0x7f020001;
+        public static final int buttonstyle=0x7f020002;
+        public static final int cupborder=0x7f020003;
+        public static final int ic_launcher=0x7f020004;
+        public static final int skaqkrtk_1285=0x7f020005;
     }
     public static final class id {
         public static final int EditText01=0x7f090009;
         public static final int EditText02=0x7f09000c;
         public static final int TextView01=0x7f09000a;
         public static final int TextView02=0x7f09000b;
-        public static final int action_settings=0x7f09000f;
+        public static final int TextView03=0x7f090017;
+        public static final int action_settings=0x7f090019;
         public static final int button1=0x7f090000;
         public static final int button2=0x7f090002;
         public static final int button3=0x7f090001;
         public static final int editText1=0x7f090007;
-        public static final int imageView1=0x7f09000e;
+        public static final int editText2=0x7f090016;
+        public static final int editText3=0x7f090018;
+        public static final int imageView1=0x7f090010;
         public static final int layout1=0x7f090005;
-        public static final int linearLayout1=0x7f09000d;
+        public static final int linearLayout1=0x7f090015;
         public static final int listView1=0x7f090004;
+        public static final int ratingBar1=0x7f09000e;
+        public static final int relativeLayout1=0x7f090011;
         public static final int textView1=0x7f090003;
         public static final int textView2=0x7f090008;
+        public static final int textView2s=0x7f09000d;
+        public static final int textView3=0x7f09000f;
+        public static final int textView4=0x7f090013;
+        public static final int textView5=0x7f090012;
+        public static final int textView6=0x7f090014;
         public static final int view1=0x7f090006;
     }
     public static final class layout {
@@ -50,23 +61,31 @@ public final class R {
         public static final int createdrinklistpage=0x7f030001;
         public static final int createdrinkpage=0x7f030002;
         public static final int drinkadvancedsearchpage=0x7f030003;
-        public static final int drinkhistorypage=0x7f030004;
-        public static final int drinkitem=0x7f030005;
-        public static final int drinkmakepage=0x7f030006;
-        public static final int drinknamesearchpage=0x7f030007;
-        public static final int drinksearchpage=0x7f030008;
-        public static final int materiallistpage=0x7f030009;
-        public static final int premakepage=0x7f03000a;
+        public static final int drinkevaluatepage=0x7f030004;
+        public static final int drinkhistorypage=0x7f030005;
+        public static final int drinkinfopage=0x7f030006;
+        public static final int drinkitem=0x7f030007;
+        public static final int drinkmakepage=0x7f030008;
+        public static final int drinknamesearchpage=0x7f030009;
+        public static final int drinksearchpage=0x7f03000a;
+        public static final int inputdrinkinfopage=0x7f03000b;
+        public static final int makedrinkpage=0x7f03000c;
+        public static final int materiallistpage=0x7f03000d;
+        public static final int premakepage=0x7f03000e;
     }
     public static final class menu {
-        public static final int main=0x7f080000;
-        public static final int material_list_page=0x7f080001;
-        public static final int pre_make_page=0x7f080002;
+        public static final int drink_info_page=0x7f080000;
+        public static final int input_drink_info_page=0x7f080001;
+        public static final int main=0x7f080002;
+        public static final int material_list_page=0x7f080003;
+        public static final int pre_make_page=0x7f080004;
     }
     public static final class string {
         public static final int action_settings=0x7f060001;
         public static final int app_name=0x7f060000;
         public static final int hello_world=0x7f060002;
+        public static final int title_activity_drink_info_page=0x7f060005;
+        public static final int title_activity_input_drink_info_page=0x7f060006;
         public static final int title_activity_material_list_page=0x7f060004;
         public static final int title_activity_pre_make_page=0x7f060003;
     }
@@ -97,7 +116,9 @@ public final class R {
  All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f070001;
-        public static final int TitleFont=0x7f070002;
+        public static final int SmallTextStyle=0x7f070003;
+        public static final int TextStyle=0x7f070002;
+        public static final int TitleFont=0x7f070004;
     }
     public static final class xml {
         public static final int device_filter=0x7f040000;
--- EpicShakerApp/src/test/abc/InputDrinkInfoPage.java
+++ EpicShakerApp/src/test/abc/InputDrinkInfoPage.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class InputDrinkInfoPage extends Activity {
 
@@ -18,6 +19,16 @@ public class InputDrinkInfoPage extends Activity {
 		btn1.setOnClickListener(new Button.OnClickListener() {
 			public void onClick(View v) {
 				Intent intetn2 = new Intent(InputDrinkInfoPage.this , CreateDrinkPage.class);
+				
+				EditText nameText=(EditText)findViewById(R.id.editText1);
+				EditText desText=(EditText)findViewById(R.id.editText2);
+				EditText tagText=(EditText)findViewById(R.id.editText3);
+
+				intetn2.putExtra("name",nameText.getText().toString());
+				intetn2.putExtra("des",desText.getText().toString());
+				intetn2.putExtra("tag",tagText.getText().toString());
+
+				
 				startActivity(intetn2);
 			}
 		});
