0d9f2be4e84c2d21688800646b6821ae8df89eb3
dimitree <jaurim1216@gmail.com>
2013-11-24 14:44:23 +0900

    key값 넘겨주는 부분 추가

--- EpicShakerApp/src/test/abc/DrinkNameSearchPage.java
+++ EpicShakerApp/src/test/abc/DrinkNameSearchPage.java
@@ -110,6 +110,8 @@ public class DrinkNameSearchPage extends Activity   {
 	public void sendDataToDrinkInfo(int position) {		
 		Intent intent = new Intent(getBaseContext(), DrinkInfoPage.class);
 		intent.putExtra("JSONstr",adapter.getItem(position).getJSON().toString());
+		intent.putExtra("id",adapter.getItem(position).getKey());
+
 		startActivity(intent);
 	}
 	
@@ -119,24 +121,19 @@ public class DrinkNameSearchPage extends Activity   {
 		
 		String theJsonString = json;
 		
-		// 선언한 리스트뷰에 사용할 리스뷰연결
+		
 		listview = (ListView) findViewById(R.id.listView1);
 
-		// 객체를 생성합니다
+	
 		alist = new ArrayList<CData>();
 
-		// 데이터를 받기위해 데이터어댑터 객체 선언
+		
 		adapter = new DataAdapter(this, alist);
 
-		// 리스트뷰에 어댑터 연결
+
 		listview.setAdapter(adapter);
 
-		// ArrayAdapter를 통해서 ArrayList에 자료 저장
-		// 하나의 String값을 저장하던 것을 CData클래스의 객체를 저장하던것으로 변경
-		// CData 객체는 생성자에 리스트 표시 텍스트뷰1의 내용과 텍스트뷰2의 내용 그리고 사진이미지값을 어댑터에 연결
 
-		// CData클래스를 만들때의 순서대로 해당 인수값을 입력
-		// 한줄 한줄이 리스트뷰에 뿌려질 한줄 한줄이라고 생각하면 됩니다.
 
 		Map<String, JSONObject> map = new HashMap<String, JSONObject>();
 		try {
@@ -153,7 +150,7 @@ public class DrinkNameSearchPage extends Activity   {
 				String key = (String) iterator.next();
 
 				adapter.add(new CData(getApplicationContext(), map.get(key).getString("name"),
-						map.get(key).getString("description"), 1,map.get(key)));
+						map.get(key).getString("description"), 1,map.get(key),key));
 
 			}
 		} catch (JSONException e) {
@@ -240,9 +237,9 @@ public class DrinkNameSearchPage extends Activity   {
 		private String m_szData;
 		private int m_szData2;
 		private JSONObject jsonObj;
-
+		private String keyData;
 		public CData(Context context, String p_szLabel, String p_szDataFile,
-				int p_szData2, JSONObject json) {
+				int p_szData2, JSONObject json, String key) {
 
 			m_szLabel = p_szLabel;
 
@@ -250,10 +247,15 @@ public class DrinkNameSearchPage extends Activity   {
 
 			m_szData2 = p_szData2;
 			
-
+			keyData = key;
+			
 			jsonObj = json;
 		}
 
+		public String getKey() {
+			return keyData;
+		}
+		
 		public JSONObject getJSON() {
 			return jsonObj;
 		}
